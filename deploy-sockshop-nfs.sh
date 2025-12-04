eb6e43efe678d48efbe04b501d6ed87e0f436d3e
root <root@instance-20251118-195734.us-central1-f.c.final-cloud-project-478619.internal>
2025-12-04 20:04:06 +0000

    Finished NFS stuff

--- deploy-sockshop-nfs.sh
+++ deploy-sockshop-nfs.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 set -e
 
-echo ">>> Creating host storage directories..."
-sudo mkdir -p /data/kind/nfs
-sudo chmod 777 /data/kind/nfs
-
 echo ">>> Deleting old cluster (if exists)..."
 kind delete cluster || true
 
-sudo rm -rf /data/kind/nfs
+sudo rm -rf /data/kind/pv-controller
+sudo rm -rf /data/kind/pv-node1
+sudo rm -rf /data/kind/pv-node2
+
+echo ">>> Creating host storage directories..."
+sudo mkdir -p /data/kind/pv-controller
+sudo chmod 777 /data/kind/pv-controller
+sudo mkdir -p /data/kind/pv-node1
+sudo chmod 777 /data/kind/pv-node1
+sudo mkdir -p /data/kind/pv-node2
+sudo chmod 777 /data/kind/pv-node2
+
+sudo rm -rf /data/nfs/*
 
 echo ">>> Creating kind cluster from your config..."
 kind create cluster --config kind-config.yaml
@@ -23,18 +31,23 @@ kubectl get nodes --no-headers | awk '/worker/ {print $1}' | while read NODE; do
   kubectl label node "$NODE" node-role.kubernetes.io/worker="" --overwrite
 done
 
-echo ">>> Installing local-path-provisioner..."
-kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+#echo ">>> Installing local-path-provisioner..."
+#kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 
-echo ">>> Setting local-path as the default StorageClass..."
-kubectl get storageclass -o name | while read sc; do
-  kubectl annotate "$sc" storageclass.kubernetes.io/is-default-class- --overwrite || true
-done
+#echo ">>> Setting local-path as the default StorageClass..."
+#kubectl get storageclass -o name | while read sc; do
+#  kubectl annotate "$sc" storageclass.kubernetes.io/is-default-class- --overwrite || true
+#done
 
-kubectl annotate storageclass local-path storageclass.kubernetes.io/is-default-class=true --overwrite
+#kubectl annotate storageclass local-path storageclass.kubernetes.io/is-default-class=true --overwrite
+
+echo ">>> Setting up NFS provisioner..."
+kubectl apply -f nfs/nfs-provisioner.yaml
+kubectl apply -f nfs/nfs-rbac.yaml
+kubectl apply -f nfs/nfs-storage-class.yaml
 
 echo ">>> Deploying Sock-Shop..."
-kubectl apply -k sock-shop-demo/manifests/overlays/single
+kubectl apply -k manifests/overlays/double
 
 echo ">>> Waiting for pods to become Ready..."
 kubectl wait --for=condition=Ready pods --all --timeout=300s || true
